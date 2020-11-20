ef0c74bd0750550ad67de0f0eab5e477e1ff353f
Alex Withington-Smith <alex@awithsmith.com>
2020-11-20 11:36:12 +0000

    added comments that display

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 require './database_connection_setup'
 require './lib/bookmark'
+require './lib/comment'
 require 'uri'
 require 'sinatra/flash'
 
@@ -48,8 +49,7 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/bookmarks/:id/comments' do
-    connection = PG.connect(dbname: 'bookmark_manager_test')
-    connection.exec("INSERT INTO comments (text, bookmark_id) VALUES('#{params[:comment]}, '#{params[:id]}');")
+    Comment.create(text: params[:comment], bookmark_id: params[:id].to_i)
     redirect('/bookmarks')
   end
 
---
+++ spec/comment_spec.rb
@@ -0,0 +1,15 @@
+require 'comment'
+require 'bookmark'
+
+describe Comment do
+  describe '.create' do
+    it "creates a new comment" do
+      bookmark = Bookmark.create(url: "http://www.makersacademy.com", title: "Makers Academy")
+      comment = Comment.create(text: 'This is a test comment', bookmark_id: bookmark.id)
+      result = DatabaseConnection.query("SELECT * FROM comments WHERE id = '#{comment.id}';")
+      expect(result.first['id']).to eq(comment.id)
+      expect(result.first['bookmark_id']).to eq(bookmark.id)
+      expect(result.first['text']).to eq(comment.text)
+    end
+  end
+end
